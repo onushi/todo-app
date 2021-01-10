4ca15ef07ad0b9c712f5e00445bba1ae4a9cc291
onushi <4374999+onushi@users.noreply.github.com>
2021-01-10 13:45:56 +0900

    新規タスク作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,22 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   get 'tasks/index'
   get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
