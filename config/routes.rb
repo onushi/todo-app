4718a1aaa98f1a1d254192194fe21fc768cff176
onushi <4374999+onushi@users.noreply.github.com>
2021-01-10 13:47:47 +0900

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks', to: 'tasks#index'
-  post 'tasks', to: 'tasks#create'
+  root 'tasks#index'
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
