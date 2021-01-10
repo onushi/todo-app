34ec33a58e6e9ee1bd04a87a2f043f3c28f429b9
onushi <4374999+onushi@users.noreply.github.com>
2021-01-10 14:05:57 +0900

    タスク編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,6 +14,16 @@ class TasksController < ApplicationController
   def create
     @task = Task.create(task_params)
     redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_url
   end
 
   private
